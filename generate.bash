8b04fc2286c7a42bca86422d4ec5668623676567
hacker1024 <hacker1024@users.sourceforge.net>
2021-02-06 23:54:43 +1100

    Add custom entry support (starting with the .idea directory)

--- generate.bash
+++ generate.bash
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-function line_break {
-  echo "" >> .gitignore
-}
-
 function append_github_global {
-  echo "# -- $2 --" >> .gitignore
+  echo -e "\n# -- $2 --" >> .gitignore
   curl -s https://raw.githubusercontent.com/github/gitignore/master/Global/$1.gitignore >> .gitignore
   echo "Appended $2."
 }
 
-> .gitignore
+cp .custom.gitignore .gitignore 
 append_github_global 'macOS' 'macOS'
-line_break
 append_github_global 'Linux' 'Linux'
-line_break
 append_github_global 'Windows' 'Windows'
-line_break
 append_github_global 'JetBrains' 'JetBrains IDEs'
-line_break
 append_github_global 'VisualStudioCode' 'Visual Studio Code'
-line_break
 append_github_global 'Xcode' 'Xcode'
